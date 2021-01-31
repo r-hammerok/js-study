fb9eccc75739c22487094928b2fba5b76da16fba
Roman Ivannikov <roman.y.ivannikov@gmail.com>
2021-02-01 05:55:25 +0700

    refactor(js/script.js): edits according to recommendations

--- lesson1/js/script.js
+++ lesson1/js/script.js
@@ -1,23 +1,15 @@
-let money, 
-income,
-addExpenses,
-deposit,
-mission,
-period;
+const 
+  money = 60000,
+  income = 'биржа',
+  addExpenses = 'Животные, Кофе, Проезд',
+  deposit = false,
+  mission = 100000,
+  period = 12,
+  budgetDay = money / 30;
 
-money = 60000;
-income = 'биржа';
-addExpenses = 'Животные, Кофе, Проезд';
-deposit = false;
-mission = 100000;
-period = 12;
-
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
+console.log(typeof money, typeof income, typeof deposit);
 console.log(addExpenses.length);
 console.log('Период равен ' + period + ' месяцев');
 console.log('Цель заработать ' + mission + ' рублей');
 console.log(addExpenses.toLowerCase().split(', '));
-let budgetDay = money / 30;
 console.log(budgetDay);
