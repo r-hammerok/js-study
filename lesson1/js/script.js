47f073a30ef7066c9ca40294e76070e343a83bdf
Roman Ivannikov <roman.y.ivannikov@gmail.com>
2021-01-31 10:06:26 +0700

    refactor(js/script.js): completed lesson2 normal

--- lesson1/js/script.js
+++ lesson1/js/script.js
@@ -5,6 +5,19 @@ deposit,
 mission,
 period;
 
-alert('Мой первый код!');
+money = 60000;
+income = 'биржа';
+addExpenses = 'Животные, Кофе, Проезд';
+deposit = false;
+mission = 100000;
+period = 12;
 
-console.log('Мой первый код в консоли!');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+console.log(addExpenses.toLowerCase().split(', '));
+let budgetDay = money / 30;
+console.log(budgetDay);
