fb2380b4259e5b416f06eb5590dee1e19b95fbe0
Roman Ivannikov <roman.y.ivannikov@gmail.com>
2021-02-01 06:01:19 +0700

    refactor(js/script.js): edits according to recommendations

--- lesson2/js/script.js
+++ lesson2/js/script.js
@@ -1,9 +1,8 @@
-let num = 266219;
+const num = 266219;
 
-let strNum = String(num);
 let multi = 1; // Произведение цифр числа
-for (var i = 0; i < strNum.length; i++) {
-  multi *= strNum[i];
+for (var i = 0; i < String(num).length; i++) {
+  multi *= String(num)[i];
 }
 multi **= 3;
 
